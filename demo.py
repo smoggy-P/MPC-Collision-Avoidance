35cf6f4b5665cdd690cd66d4e88193823bf24b69
smoggy-P <smoggy@sjtu.edu.cn>
2022-03-25 11:47:16 +0100

    bug fixed

--- demo.py
+++ demo.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     x_target[2] = 2
     x_next = x_init
     A,b=convexify(x_init[:2].flatten(),0.5,obstacle_list)
-    inter_goal=get_intermediate_goal(x_init[:2].flatten(),0.5, x_target[:2].flatten(), A,b).flatten()
+    inter_goal=get_intermediate_goal(x_init[:2].flatten(), x_target[:2].flatten(), A,b).flatten()
     x_intergoal=np.zeros(10)
     x_intergoal[:2]=inter_goal
     x_intergoal[2]=2
@@ -31,7 +31,7 @@ if __name__ == "__main__":
         x_next = quadrotor.next_x(x_next, u)
         
         A,b=convexify(x_next[:2].flatten(),0.5,obstacle_list)
-        inter_goal=get_intermediate_goal(x_next[:2].flatten(),0.5, x_target[:2].flatten(), A,b).flatten()
+        inter_goal=get_intermediate_goal(x_next[:2].flatten(), x_target[:2].flatten(), A,b).flatten()
         x_intergoal=np.zeros(10)
         x_intergoal[:2]=inter_goal
         x_intergoal[2]=2
