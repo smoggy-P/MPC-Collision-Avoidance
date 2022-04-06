6ea1a6a12f95f49964c057546d8e8e59a51e5ca2
benjamin.piet <benjamin.piet@numericable.fr>
2022-04-06 15:13:31 +0200

    terminal set test and representation

--- test.py
+++ test.py
@@ -99,3 +99,63 @@ L = control.place(A_tilde.T, C_tilde.T, eigs).T
 print("L=",L)
 eig_val,eig_vec=np.linalg.eig(A_tilde-L@C_tilde)
 print("eigen values of A-LC : ", eig_val)
+
+######################## Test for terminal set
+def get_terminal_set_corners(quadrotor_linear,goal,cst=0.0037):
+    input_ub=0.07
+    input_lb=-0.07
+    Q = np.identity(10)
+    R = 0.02*np.eye(4)
+    P, L, K = control.dare(quadrotor_linear.A, quadrotor_linear.B, Q, R)
+    eig_val,eig_vec=np.linalg.eig(P)
+    
+    corner_list=[]
+    for a in range(2):
+        for b in range(2):
+            for c in range(2):
+                x = (-1)**a *eig_vec[0]/np.linalg.norm(eig_vec[0])*eig_val[0]*cst
+                x+= (-1)**b *eig_vec[1]/np.linalg.norm(eig_vec[1])*eig_val[1]*cst
+                # eigenvalues 2 and 3 are the same, so I pick the one with a stronger influence on position
+                # for better representation
+                x+= (-1)**c *eig_vec[3]/np.linalg.norm(eig_vec[3])*eig_val[3]*cst
+                corner=np.array(goal)+np.array(x)[:3]
+                corner_list.append(corner)
+    return corner_list
+    
+cst=0.0037
+
+input_ub=0.07
+input_lb=-0.07
+Q = np.identity(10)
+R = 0.02*np.eye(4)
+
+P, L, K = control.dare(quadrotor_linear.A, quadrotor_linear.B, Q, R)
+eig_val,eig_vec=np.linalg.eig(P)
+
+
+for a in range(2):
+    for b in range(2):
+        for c in range(2):
+            for d in range(2):
+                for e in range(2):
+                    for f in range(2):
+                        for g in range(2):
+                            for h in range(2):
+                                for i in range(2):
+                                    for j in range(2):
+                                        x = (-1)**a *eig_vec[0]/np.linalg.norm(eig_vec[0])*eig_val[0]*cst
+                                        x+= (-1)**b *eig_vec[1]/np.linalg.norm(eig_vec[1])*eig_val[1]*cst
+                                        x+= (-1)**c *eig_vec[2]/np.linalg.norm(eig_vec[2])*eig_val[2]*cst
+                                        x+= (-1)**d *eig_vec[3]/np.linalg.norm(eig_vec[3])*eig_val[3]*cst
+                                        x+= (-1)**e *eig_vec[4]/np.linalg.norm(eig_vec[4])*eig_val[4]*cst
+                                        x+= (-1)**f *eig_vec[5]/np.linalg.norm(eig_vec[5])*eig_val[5]*cst
+                                        x+= (-1)**g *eig_vec[6]/np.linalg.norm(eig_vec[6])*eig_val[6]*cst
+                                        x+= (-1)**h *eig_vec[7]/np.linalg.norm(eig_vec[7])*eig_val[7]*cst
+                                        x+= (-1)**i *eig_vec[8]/np.linalg.norm(eig_vec[8])*eig_val[8]*cst
+                                        x+= (-1)**j *eig_vec[9]/np.linalg.norm(eig_vec[9])*eig_val[9]*cst
+                                        #print("***")
+                                        print(np.dot(K,x)<=input_ub)
+                                        #if np.any(K@x<input_lb):
+                                        #    print(a,b,c,d,e,f,g,h,i,j)
+                                        print(np.dot(K,x)>=input_lb)
+                                        
